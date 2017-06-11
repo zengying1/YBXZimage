c85ae616edf4cef2a44c3bacb2a6eeeeed8f97a3
Oreal51 <Oreal51@oreal51demacbook-pro.local>
2017-06-11 15:33:28 +0800

    Initial Commit

---
+++ ybxzicon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ybxzicon
+//
+//  Created by Oreal51 on 2017/6/11.
+//  Copyright © 2017年 Oreal51. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ybxzicon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ybxzicon
+//
+//  Created by Oreal51 on 2017/6/11.
+//  Copyright © 2017年 Oreal51. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
